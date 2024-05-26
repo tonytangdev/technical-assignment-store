1ff7392ba03967a5501f265fbd25d121e867e415
tonytang-planity <tony.tang@planity.com>
2024-05-26 09:43:43 +0200

    Pass UserStore class - Basic Operations tests

--- src/store.ts
+++ src/store.ts
@@ -20,26 +20,53 @@ export interface IStore {
   entries(): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
+const permissions = new Map<string, Permission>();
+
+export function Restrict(
+  permission: Permission = "none"
+): (target: Object, propertyKey: string | symbol) => void {
+  return function (target: Object, propertyKey: string | symbol) {
+    const key = generateKey(target, propertyKey);
+    permissions.set(key, permission);
+  };
+}
+
+function getPermission(
+  target: Object,
+  propertyKey: string | symbol
+): Permission | undefined {
+  const key = generateKey(target, propertyKey);
+  return permissions.get(key);
+}
+
+function generateKey(target: Object, propertyKey: string | symbol) {
+  return `${target.constructor.name}.${String(propertyKey)}`;
 }
 
 export class Store implements IStore {
+  private store: StoreValue = {};
   defaultPolicy: Permission = "rw";
 
   allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const permission = permissions.get(key) ?? this.defaultPolicy;
+    return permission.includes("r");
   }
 
   allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const permission = permissions.get(key) ?? this.defaultPolicy;
+    return permission.includes("w");
   }
 
   read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
+    const storeObject = this.store as JSONObject;
+    let valueAtPath = storeObject[path] as StoreResult;
+    return valueAtPath;
   }
 
   write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
+    const storeObject = this.store as JSONObject;
+    (storeObject[path] as StoreValue) = value;
+    return value;
   }
 
   writeEntries(entries: JSONObject): void {
